80e6fa5280bb699d0fc4f4cb713f32b6af6a6ba0
ccooper5000 <info@athensintech.com>
2025-09-23 22:27:14 -0500

    chore: add clsx and tailwind-merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from 'clsx';
+// lib/utils.ts
+import { type ClassValue } from 'clsx';
+import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Tailwind-safe className combiner.
+ * Merges conditional classes (clsx) and resolves Tailwind conflicts (twMerge).
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
